b459c3274f989aeb76dd09336b2b88cd66d5083b
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-04-16 16:25:52 -0300

    cambios

---
+++ js/header/idiom-selector.js
@@ -0,0 +1,85 @@
+function idiomSelector(){
+   
+    //idiom selector
+    let idiomSelectorButton = document.querySelector('.idiom-selector-button');
+    let idiomOptionSelection = document.querySelector('.idiom-option-selection');
+    let idiomSelectedItem = document.querySelector('.idiom-selected-item');
+    //idiom options
+    let idiomOptionsContainer = document.querySelector('.idiom-options');
+    let idiomOptions = document.querySelectorAll('.idiom-option');
+    
+    
+    idiomSelectorButton.addEventListener(
+        "click",
+        () => {
+            openIdiomSelect(
+                idiomSelectorButton,
+                idiomOptionSelection,
+                idiomOptionsContainer
+            );
+        }
+    );
+    
+    idiomOptions.forEach(
+        function(element){
+            element.addEventListener(
+                "click",
+                () => {
+                    changeSelectedIdiom( 
+                        element
+                    ); 
+                }
+            );
+        }
+
+    );
+
+
+}
+
+
+function openIdiomSelect(
+    idiomSelectorButton,
+    idiomOptionSelection,  
+    idiomOptionsContainer
+) {
+    
+    idiomOptionsContainer.classList.toggle('idiom-options-hide');
+    idiomOptionSelection.classList.toggle('idiom-selected-item-open');
+
+    if(idiomSelectorButton.getAttribute('value')==="true"){
+        idiomSelectorButton.setAttribute('value', 'false');
+    }else{
+        idiomSelectorButton.setAttribute('value', 'true');
+    }
+}
+
+function changeSelectedIdiom(element){
+
+    //selected idiom flag
+    var selectedFlagImage = document.querySelector('#selectedIdiomFlag');
+    var selectedIdiomText = document.querySelector('#selectedItemText');
+
+    //change flag and text content for the selected item in combobox
+    switch(element.getAttribute('value')){
+        case 'spanish':
+            selectedFlagImage.src = "img/flags/spain-flag.jpg";
+            selectedItemText.textContent = 'Spanish';
+        break;
+        case 'english':
+            selectedFlagImage.src= "img/flags/united-kingdom-flag.jpg";
+            selectedItemText.textContent = 'English';
+        break;
+        case 'german':
+            selectedFlagImage.src = "img/flags/deutschland-flag.png";
+            selectedItemText.textContent = 'German';
+        break;
+        default: 
+            selectedFlagImage.src = "img/flags/spain-flag.jpg";
+            selectedItemText.textContent = 'Spanish';
+            
+    }
+
+    idiomSelectedItem.setAttribute( 'value', element.getAttribute('value') );
+
+}
