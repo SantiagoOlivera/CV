0aa03e26dee055467e914a060fdd1f46e93efc0d
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-04-24 16:23:14 -0300

    cambios

---
+++ js/body.js
@@ -0,0 +1,6 @@
+//detect body events
+//var body = document.getElementsByTagName('body')[0];
+
+
+
+
--- js/header/idiom-selector.js
+++ js/header/idiom-selector.js
@@ -1,133 +1,48 @@
 //idiom selector
-let idiomSelectorContainer = document.querySelector('.idiom-selector-container');
 
-let idiomOptionSelection = document.querySelector('.idiom-option-selection');
-let idiomSelectedItem = document.querySelector('.idiom-selected-item');
-//idiom options
-let idiomOptionsContainer = document.querySelector('.idiom-options');
-let idiomOptions = document.querySelectorAll('.idiom-option');
+let idiomSelected = document.querySelector('#idiomSelected');
+let idiomSelector = document.querySelector('#idiomSelector');
+let idiomSelectorClose = document.querySelector('#closeIdiomSelector');
+let idiomItem = document.querySelectorAll('.idiom-item');
 
-
-//evento para abrir opciones del combobox
-idiomOptionSelection.addEventListener(
+idiomSelected.addEventListener(
     "click",
-    () => {
-        
-        /* openAndCloseIdiomSelect(
-            idiomOptionSelection,
-            idiomOptionsContainer
-        ); */
-    }
-);
-
-
-
-idiomSelectorContainer.addEventListener(
-    'mouseover',
     ()=>{
-        idiomSelectorContainer.style.height = "auto";
+        idiomSelector.style.display = "block";
     }
 );
-idiomSelectorContainer.addEventListener(
-    'mouseout',
+idiomSelectorClose.addEventListener(
+    "click",
     ()=>{
-        idiomSelectorContainer.style.height = "91px";
+        idiomSelector.style.display = "none";
     }
 );
 
-idiomSelectorContainer.addEventListener(
-    'click',
-    ()=>{
-        
-        idiomOptionSelection.classList.toggle('idiom-selected-item-open');
-
-        if(idiomSelectedItem.getAttribute('open')==="false"){
-            idiomSelectorContainer.style.height = "auto";
-            idiomSelectedItem.setAttribute('open','true');
-        }else{
-            idiomSelectorContainer.style.height = "91px";
-            idiomSelectedItem.setAttribute('open','false');
-        }
-        
-
-    }
-);
-
-//evento para modificar imagen bandera y nombre del idioma 
-idiomOptions.forEach(
-    function(element){
+idiomItem.forEach(
+    function(element) {
         element.addEventListener(
             "click",
-            () => {
-                changeSelectedIdiom( 
-                    element, 
-                    idiomSelectedItem
-                );
-                closeIdiomSelect(idiomSelectorContainer);
-
+            ()=>{
+                //change idiom flag and text 
+                changeIdiom(element);
+                //close idiom  selector 
+                idiomSelector.style.display = "none";
             }
         );
     }
-
 );
 
- function closeIdiomSelect(idiomSelectorContainer){
-     
-    idiomSelectorContainer.style.height = "91px";
+function changeIdiom(element){
 
- }
+    var flag = document.querySelector('#idiomSelected .idiom-flag img');
+    var text = document.querySelector('#idiomSelected .idiom-text');
 
+    //console.log(element);
+    //console.log(element.children[0].children[0].src);
+    //console.log(element.children[1].innerText);
+    idiomSelected.setAttribute('idiom', element.getAttribute('idiom'));
+    flag.src = element.children[0].children[0].src;
+    text.textContent = element.children[1].innerText;          
 
-/* function openAndCloseIdiomSelect(
-    idiomOptionSelection,  
-    idiomOptionsContainer,
-    event
-) {
-
-
-        idiomOptionsContainer.classList.toggle('idiom-options-hide');
-        idiomOptionSelection.classList.toggle('idiom-selected-item-open');
-    
-        if(idiomOptionSelection.getAttribute('open')==="true"){
-            idiomOptionSelection.setAttribute('open', 'false');
-        }else{
-            idiomOptionSelection.setAttribute('open', 'true');
-        }
-    
-} */
-
-
-
-
-function changeSelectedIdiom(
-    element, 
-    idiomSelectedItem
-){
-
-    //selected idiom flag
-    var selectedFlagImage = document.querySelector('#selectedIdiomFlag');
-    var selectedIdiomText = document.querySelector('#selectedItemText');
-
-    //change flag and text content for the selected item in combobox
-    switch(element.getAttribute('value')){
-        case 'spanish':
-            selectedFlagImage.src = "img/flags/spain-flag.jpg";
-            selectedIdiomText.textContent = 'Spanish';
-        break;
-        case 'english':
-            selectedFlagImage.src= "img/flags/united-kingdom-flag.jpg";
-            selectedIdiomText.textContent = 'English';
-        break;
-        case 'german':
-            selectedFlagImage.src = "img/flags/deutschland-flag.png";
-            selectedIdiomText.textContent = 'German';
-        break;
-        default: 
-            selectedFlagImage.src = "img/flags/spain-flag.jpg";
-            selectedIdiomText.textContent = 'Spanish';
-            
-    }
-
-    idiomSelectedItem.setAttribute( 'value', element.getAttribute('value') );
-    
 }
+
