a58ca00da6c0199f5fcb0e6ae45f0482d5eb65fd
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-04-25 15:03:49 -0300

    cambios

--- js/header/personal-data.js
+++ js/header/personal-data.js
@@ -2,7 +2,7 @@ function getPersonalData(person){
     //object person
     var age = person.getAge();
     //console.log(age);
-    document.querySelector('#age').textContent = 'Edad: ' + age + ' años';
+    document.querySelector('#age').textContent =  age;
 }
 
 
