830766d289de1306ce888866f7a96ae0957e21b6
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-04-30 18:32:17 -0300

    cambios

--- js/nav/open-and-close-nav.js
+++ js/nav/open-and-close-nav.js
@@ -1,5 +1,6 @@
 var openAndCloseNav = document.querySelector('#openAndCloseNav');
-var navContainer = document.querySelector('.nav-container');
+var navContainer = document.querySelector('.nav');
+var menuContainer =document.querySelector('.menu-container');
 //var nav = document.querySelector('.nav');
 //var navHeaderOpenClose = document.querySelector('.nav-header-open-close');
 
@@ -9,15 +10,20 @@ openAndCloseNav.addEventListener(
         
         //navContainer.classList.toggle('nav-container-open');
         if(navContainer.getAttribute("open")==="true"){
-            navContainer.style.width = "0";
+            navContainer.style.width = 0;
+            menuContainer.style.position = "static";
+            menuContainer.style.width = 0;
             navContainer.setAttribute("open", "false");
         }else{
+            
+            menuContainer.style.position = "fixed";
+            menuContainer.style.width = "30%";
+
             if(window.innerWidth <= 700){
-                navContainer.style.width = "100%";
+                navContainer.style.display = "100%";
             }else{
                 navContainer.style.width = "33.33%";
             }
-
             navContainer.setAttribute("open", "true");
         }
 
