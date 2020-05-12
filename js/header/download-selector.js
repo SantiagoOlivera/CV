7ec9eafc3896b37e1c89688aa6193546312acb64
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-05-11 22:21:33 -0300

    cambio

--- js/header/download-selector.js
+++ js/header/download-selector.js
@@ -1,12 +1,20 @@
 let downloadSelector = document.querySelector('#downloadSelector');
 let downloadSelectorButton = document.querySelector('#downloadSelectorButton');
+let downloadCloseButton = document.querySelector('#downloadCloseButton');
 
 downloadSelectorButton.addEventListener(
     "click",
     () => {
-        
         if(window.innerWidth < 700 ){
-            downloadSelector.classList.toggle('show');
+            
+            downloadSelector.classList.add('show');
+            
         }
+    }
+);
+downloadCloseButton.addEventListener(
+    "click",
+    () => {
+         downloadSelector.classList.remove('show');
     }
 );
