a808a2f72ce01c0456de81877b99da9d3d197f97
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-06-08 20:46:47 -0300

    cambios

--- js/content/myBetterSkills.js
+++ js/content/myBetterSkills.js
@@ -4,5 +4,69 @@ var myBetterSkills = document.querySelector('#myBetterSkills');
 
 
 function setMyBetterSkillsItemsHTMLText(myBetterSkillsItems){
-    console.log(myBetterSkills);
+
+    var myBetterSkillsHTMLText = '';
+
+    var cont = 1;
+
+    myBetterSkillsItems.forEach(
+
+    
+
+        element => {
+        
+            myBetterSkillsHTMLText = ` ${myBetterSkillsHTMLText} 
+
+                    <div class="my-better-skills">
+
+                    <div class="my-better-skills-container">
+
+                        <div class="my-better-skills-item">
+
+                            <div class="my-better-skills-icon">
+                                <h1>${cont++}</h1>
+                            </div>
+
+                            <div class="my-better-skills-item-container">
+
+                                
+
+                                <div class="my-better-skills-title">
+                                    <h1>
+                                        <span idTranslate="${element.title[1]}">
+                                            ${element.title[0]} 
+                                        </span>
+                                    </h1>
+                                </div>
+                                <div class="my-better-skills-description">
+                                    <p>
+                                        <span idTranslate="${element.description[1]}">
+                                            ${element.description[0]}
+                                        </span>
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+                    </div>
+
+                </div>
+            `;
+    });
+
+
+    myBetterSkills.innerHTML = `
+        
+        <h1>
+            <span idTranslate="14">
+                My Better Skills
+            </span>
+        </h1>
+
+        ${myBetterSkillsHTMLText}
+
+    `; 
+
 }
