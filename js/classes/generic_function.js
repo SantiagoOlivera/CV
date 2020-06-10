659cdad1e8c4f8a092871d99d2d638a8bb6296a8
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-06-10 19:07:58 -0300

    cambios

--- js/classes/generic_function.js
+++ js/classes/generic_function.js
@@ -126,3 +126,50 @@ class GenericFunction{
 
 
 }
+
+
+monthsNames = {
+
+    spanish: {
+        1:  'Enero',
+        2:  'Febrero',
+        3:  'Marzo',
+        4:  'Abril',
+        5:  'Mayo',
+        6:  'Junio',
+        7:  'Julio',
+        8:  'Agosto',
+        9:  'Septiembre',
+        10: 'Octubre',
+        11: 'Noviembre',
+        12: 'Diciembre',
+    } ,
+    english: {
+        1:  'Jenuary',
+        2:  'Febrery',
+        3:  'March',
+        4:  'April',
+        5:  'May',
+        6:  'June',
+        7:  'July',
+        8:  'August',
+        9:  'September',
+        10: 'Octuber',
+        11: 'November',
+        12: 'December',
+    } ,
+    german: {
+        1:  'Januar',
+        2:  'Februar',
+        3:  'März',
+        4:  'April',
+        5:  'Mai',
+        6:  'Juni',
+        7:  'Juli',
+        8:  'August',
+        9:  'September',
+        10: 'Oktober',
+        11: 'November',
+        12: 'Dezember',
+    } 
+}
