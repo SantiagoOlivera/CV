192ba0bbb249413b692c55f33b20c5e82fcf5018
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-06-13 13:29:45 -0300

    cambios

--- js/header/personal-data.js
+++ js/header/personal-data.js
@@ -3,8 +3,23 @@ function getPersonalData(person){
     var age = person.getAge();
     //console.log(age);
     document.querySelector('#age').textContent =  age;
-    document.querySelector('#name').textContent =  'Santiago Ariel Olivera';
+    document.querySelector('#name').textContent = `${person.name} ${person.lastname}` ;
 }
 
 
-    
+var personalDataWindow = document.querySelector('#personalData');
+var personalDataButton = document.querySelector('#personalDataImage');
+
+console.log(personalDataWindow);
+console.log(personalDataButton);
+
+personalDataButton.addEventListener(
+    'click', () => {
+        console.log("asd");
+        //openPersonalDataWindow();
+    }
+);
+
+function openPersonalDataWindow(){
+    personalDataWindow.classList.add('show');
+}
